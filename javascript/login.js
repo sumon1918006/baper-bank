363e76588f8ca0d1643c3a6b464f592aabea9721
sumon06 <ohidulislamsumon06@gmail.com>
2024-05-29 15:12:10 +0600

    first commit

---
+++ javascript/login.js
@@ -0,0 +1,16 @@
+document.getElementById('btn-submit').addEventListener('click', function(){
+
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+   
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    
+ 
+    if(email === 'sontan@baap.com' && password === 'secret'){
+        window.location.href = 'bank.html';
+    }
+    else{
+        alert('Tui password vule gesos!! Tor aj theke vat bondho.')
+    }
+})
